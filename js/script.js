2fe93673b0d60c5ddbbcf2d767bc25e0dfa07df0
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-04-26 22:05:35 -0500

    List added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 //Variables
 const taskInput = document.querySelector(".task-input input");
 let todos = JSON.parse(localStorage.getItem("todo-list"));    // <-- We retrieve the value of the todos
-
+const taskBox = document.querySelector(".task-box");
 
 //Listeners
 document.addEventListener("DOMContentLoaded", showTodo);
 
 function showTodo(){
     todos.forEach( (todo, id) => {   // <-- if there are two parameters the second one is an autoincrement value
-        
-    
+        const li = document.createElement("li");
+        li.classList.add("task");
+        li.innerHTML = `
+                <div class="task__field">
+                    <label for=""></label>
+                    <input type="checkbox" name="" id="1">
+                    <p>${todo.name}</p>
+                </div>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="task-menu">
+                        <li class="uil uil-pen">Edit</li>
+                        <li class="uil uil-trash">Delete</li>
+                    </ul>
+                </div>       
+        `;
+        taskBox.prepend(li);
     });
 }
 
