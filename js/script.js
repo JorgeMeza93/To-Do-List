280d970bcda4a00cf408df19cccb17bcdc787be3
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-04-27 17:02:06 -0500

    Modificaciones al Script

--- js/script.js
+++ js/script.js
@@ -7,25 +7,29 @@ const taskBox = document.querySelector(".task-box");
 document.addEventListener("DOMContentLoaded", showTodo);
 
 function showTodo(){
-    todos.forEach( (todo, id) => {   // <-- if there are two parameters the second one is an autoincrement value
-        const li = document.createElement("li");
-        li.classList.add("task");
-        li.innerHTML = `
-                <div class="task__field">
-                    <label for=""></label>
-                    <input type="checkbox" name="" id="1">
-                    <p>${todo.name}</p>
-                </div>
-                <div class="settings">
-                    <i class="uil uil-ellipsis-h"></i>
-                    <ul class="task-menu">
-                        <li class="uil uil-pen">Edit</li>
-                        <li class="uil uil-trash">Delete</li>
-                    </ul>
-                </div>       
-        `;
-        taskBox.prepend(li);
-    });
+    if(todos){
+        clearHTML();
+        todos.forEach( (todo, id) => {   // <-- if there are two parameters the second one is an autoincrement value
+            const li = document.createElement("li");
+                li.classList.add("task"); 
+                li.innerHTML = `
+                    <div class="task__field">
+                        <label for=""></label>
+                        <input onclick="updatedStatus(this)" type="checkbox" name="" id="1">    <!-- this just makes reference to the same objet checkbox-->
+                        <p>${todo.name}</p>
+                    </div>
+                    <div class="settings">
+                        <i class="uil uil-ellipsis-h"></i>
+                        <ul class="task-menu">
+                            <li class="uil uil-pen">Edit</li>
+                            <li class="uil uil-trash">Delete</li>
+                        </ul>
+                    </div>       
+            `;
+            taskBox.prepend(li);
+    
+        });
+    }
 }
 
 taskInput.addEventListener("keyup", e => {
@@ -39,8 +43,22 @@ taskInput.addEventListener("keyup", e => {
         localStorage.setItem("todo-list", JSON.stringify(todos)); // <-- Local storage only storage string so we need to convert the array into a string
         showTodo();
      }
-
+     
 });
-function saludar(){
-    console.log("Y se durmió, como estás");
+function clearHTML(){
+    while(taskBox.firstChild){  // <-- While there's something inside the ul remove the first child
+        taskBox.removeChild(taskBox.firstChild);
+    }
+}
+
+function updatedStatus(selectedTask){
+    let taskName = selectedTask.parentElement.lastElementChild;
+    console.log(taskName);
+    if(selectedTask.checked){
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    }
+    else{
+        taskName.classList.remove("checked");
+    }
 }
